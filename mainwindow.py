4901711759aa03a31412194c2117319f06b3b5ee
nunopython <nunosilvalmeida@gmail.com>
2018-02-03 14:18:53 +0100

    window layout and code annotation

--- mainwindow.py
+++ mainwindow.py
@@ -4,104 +4,99 @@ import datetime
 import pandas as pd
 from PyQt5 import QtCore, QtGui, QtWidgets
 
+
 class Ui_MainWindow(object):
+
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(803, 518)
+        MainWindow.resize(900, 600)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.gridLayoutWidget = QtWidgets.QWidget(self.centralwidget)
-        self.gridLayoutWidget.setGeometry(QtCore.QRect(20, 20, 771, 475))
-        self.gridLayoutWidget.setObjectName("gridLayoutWidget")
-        self.gridLayout = QtWidgets.QGridLayout(self.gridLayoutWidget)
-        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout_2 = QtWidgets.QGridLayout(self.centralwidget)
+        self.gridLayout_2.setObjectName("gridLayout_2")
+        self.gridLayout = QtWidgets.QGridLayout()
         self.gridLayout.setObjectName("gridLayout")
-        self.label_2 = QtWidgets.QLabel(self.gridLayoutWidget)
-        self.label_2.setObjectName("label_2")
-        self.gridLayout.addWidget(self.label_2, 0, 2, 1, 1)
-        self.dateExit = QtWidgets.QDateEdit(self.gridLayoutWidget)
+        self.dateExit = QtWidgets.QDateEdit(self.centralwidget)
         self.dateExit.setDateTime(QtCore.QDateTime(QtCore.QDate(2017, 1, 1), QtCore.QTime(0, 0, 0)))
         self.dateExit.setObjectName("dateExit")
-        self.gridLayout.addWidget(self.dateExit, 0, 3, 1, 1)
-        self.dateEdit = QtWidgets.QDateEdit(self.gridLayoutWidget)
-        self.dateEdit.setDateTime(QtCore.QDateTime(QtCore.QDate(2017, 1, 1), QtCore.QTime(0, 0, 0)))
-        self.dateEdit.setObjectName("dateEdit")
-        self.gridLayout.addWidget(self.dateEdit, 0, 9, 1, 1, QtCore.Qt.AlignLeft)
-        self.dateEntry = QtWidgets.QDateEdit(self.gridLayoutWidget)
-        self.dateEntry.setDateTime(QtCore.QDateTime(QtCore.QDate(2017, 1, 1), QtCore.QTime(0, 0, 0)))
-        self.dateEntry.setObjectName("dateEntry")
-        self.gridLayout.addWidget(self.dateEntry, 0, 1, 1, 1)
-        self.label = QtWidgets.QLabel(self.gridLayoutWidget)
-        self.label.setObjectName("label")
-        self.gridLayout.addWidget(self.label, 0, 0, 1, 1, QtCore.Qt.AlignLeft)
-        self.addStay = QtWidgets.QPushButton(self.gridLayoutWidget)
-        self.addStay.setObjectName("addStay")
-        self.gridLayout.addWidget(self.addStay, 0, 4, 1, 1)
-        self.line = QtWidgets.QFrame(self.gridLayoutWidget)
+        self.gridLayout.addWidget(self.dateExit, 0, 5, 1, 1)
+        self.line = QtWidgets.QFrame(self.centralwidget)
         self.line.setFrameShape(QtWidgets.QFrame.VLine)
         self.line.setFrameShadow(QtWidgets.QFrame.Sunken)
         self.line.setObjectName("line")
-        self.gridLayout.addWidget(self.line, 3, 5, 1, 1)
-        self.calculate = QtWidgets.QPushButton(self.gridLayoutWidget)
-        self.calculate.setObjectName("calculate")
-        self.gridLayout.addWidget(self.calculate, 2, 0, 1, 5)
-        self.removeStay = QtWidgets.QPushButton(self.gridLayoutWidget)
-        self.removeStay.setObjectName("removeStay")
-        self.gridLayout.addWidget(self.removeStay, 1, 9, 1, 1, QtCore.Qt.AlignTop)
-        self.listWidget_2 = QtWidgets.QListWidget(self.gridLayoutWidget)
-        self.listWidget_2.setObjectName("listWidget_2")
-        self.gridLayout.addWidget(self.listWidget_2, 3, 0, 1, 5)
-        self.listWidget = QtWidgets.QListWidget(self.gridLayoutWidget)
+        self.gridLayout.addWidget(self.line, 6, 8, 1, 1)
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setObjectName("label_2")
+        self.gridLayout.addWidget(self.label_2, 0, 4, 1, 1)
+        self.listWidget = QtWidgets.QListWidget(self.centralwidget)
         self.listWidget.setObjectName("listWidget")
-        self.gridLayout.addWidget(self.listWidget, 1, 0, 1, 9)
-        self.listWidget_3 = QtWidgets.QListWidget(self.gridLayoutWidget)
+        self.gridLayout.addWidget(self.listWidget, 2, 0, 1, 11)
+        self.calculate = QtWidgets.QPushButton(self.centralwidget)
+        self.calculate.setObjectName("calculate")
+        self.gridLayout.addWidget(self.calculate, 5, 0, 1, 8)
+        self.removeStay = QtWidgets.QPushButton(self.centralwidget)
+        self.removeStay.setObjectName("removeStay")
+        self.gridLayout.addWidget(self.removeStay, 2, 11, 1, 1, QtCore.Qt.AlignTop)
+        self.addStay = QtWidgets.QPushButton(self.centralwidget)
+        self.addStay.setObjectName("addStay")
+        self.gridLayout.addWidget(self.addStay, 0, 7, 1, 1)
+        self.listWidget_3 = QtWidgets.QListWidget(self.centralwidget)
         self.listWidget_3.setObjectName("listWidget_3")
-        self.gridLayout.addWidget(self.listWidget_3, 3, 6, 1, 4)
-        self.forecastEntry = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.gridLayout.addWidget(self.listWidget_3, 6, 9, 1, 3)
+        self.forecastEntry = QtWidgets.QPushButton(self.centralwidget)
         self.forecastEntry.setObjectName("forecastEntry")
-        self.gridLayout.addWidget(self.forecastEntry, 2, 6, 1, 4)
-        self.label_3 = QtWidgets.QLabel(self.gridLayoutWidget)
-        self.label_3.setObjectName("label_3")
-        self.gridLayout.addWidget(self.label_3, 0, 5, 1, 4, QtCore.Qt.AlignRight)
+        self.gridLayout.addWidget(self.forecastEntry, 5, 9, 1, 3)
+        self.listWidget_2 = QtWidgets.QListWidget(self.centralwidget)
+        self.listWidget_2.setObjectName("listWidget_2")
+        self.gridLayout.addWidget(self.listWidget_2, 6, 0, 1, 8)
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setObjectName("label")
+        self.gridLayout.addWidget(self.label, 0, 0, 1, 1, QtCore.Qt.AlignLeft)
+        self.dateEntry = QtWidgets.QDateEdit(self.centralwidget)
+        self.dateEntry.setDateTime(QtCore.QDateTime(QtCore.QDate(2017, 1, 1), QtCore.QTime(0, 0, 0)))
+        self.dateEntry.setObjectName("dateEntry")
+        self.gridLayout.addWidget(self.dateEntry, 0, 1, 1, 1)
+        self.resetAllEntries = QtWidgets.QPushButton(self.centralwidget)
+        self.resetAllEntries.setObjectName("resetAllEntries")
+        self.gridLayout.addWidget(self.resetAllEntries, 0, 11, 1, 1)
+        self.gridLayout_2.addLayout(self.gridLayout, 0, 0, 1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
-        MainWindow.setTabOrder(self.dateEntry, self.dateExit)
-        MainWindow.setTabOrder(self.dateExit, self.addStay)
-        MainWindow.setTabOrder(self.addStay, self.dateEdit)
-        MainWindow.setTabOrder(self.dateEdit, self.listWidget)
+        MainWindow.setTabOrder(self.addStay, self.listWidget)
         MainWindow.setTabOrder(self.listWidget, self.removeStay)
         MainWindow.setTabOrder(self.removeStay, self.calculate)
         MainWindow.setTabOrder(self.calculate, self.listWidget_2)
         MainWindow.setTabOrder(self.listWidget_2, self.forecastEntry)
         MainWindow.setTabOrder(self.forecastEntry, self.listWidget_3)
 
-        self.dates = []
+        self.dates = []  # list for periods of stay
         self.dateEntry.setFocus()
-        self.addStay.clicked.connect(self.printToList)
-        self.removeStay.clicked.connect(self.removeStayAction)
+        self.addStay.clicked.connect(self.printToList)  # add stay to list widget
+        self.removeStay.clicked.connect(self.removeStayAction)  # remove stay from list widget and list of stays
         self.calculate.clicked.connect(self.calculateStay)
         self.forecastEntry.clicked.connect(self.nextPossibleEntry)
+        self.resetAllEntries.clicked.connect(self.reset_all_entries)
 
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "Schengen stay calculator"))
         self.label_2.setText(_translate("MainWindow", "Exit:"))
-        self.label.setText(_translate("MainWindow", "Entry:"))
-        self.addStay.setText(_translate("MainWindow", "Add stay"))
-        self.calculate.setText(_translate("MainWindow", "Calculate duration of stay since first entry"))
+        self.calculate.setText(_translate("MainWindow", "Calculate total duration of stay in the Schenge area"))
         self.removeStay.setText(_translate("MainWindow", "Remove stay"))
+        self.addStay.setText(_translate("MainWindow", "Add stay"))
         self.forecastEntry.setText(_translate("MainWindow", "Date of the next possible entry"))
-        self.label_3.setText(_translate("MainWindow", "Visa valid until:"))
+        self.label.setText(_translate("MainWindow", "Entry:"))
+        self.resetAllEntries.setText(_translate("MainWindow", "Reset all entries"))
 
 
     def getDates(self):
 
         self.dateOfEntry = self.dateEntry.date()
-        self.dateIn = self.dateOfEntry.toString()
-        self.dateOfEntryPy = self.dateOfEntry.toPyDate()
+        self.dateIn = self.dateOfEntry.toString()  # for printing str in list widget
+        self.dateOfEntryPy = self.dateOfEntry.toPyDate()  # for creating date range with pandas
 
         self.dateOfExit = self.dateExit.date()  # for doing the if below
 
@@ -109,16 +104,17 @@ class Ui_MainWindow(object):
         self.dateOfExitPy = self.dateOfExit.toPyDate()
 
         if self.dateOfExit < self.dateOfEntry:
-            # Error box for condition Entry > Exit
+            # error box for condition Entry > Exit
             msgbox = QtWidgets.QMessageBox()
             msgbox.setText('Date of exit must be after date of entry.')
             msgbox.exec_()
             return False  # To be picked up by def printToList
         elif self.dateOfExitPy > datetime.date.today():
+            # error box for condition Exit > today
             msgbox = QtWidgets.QMessageBox()
             msgbox.setText('Date of exit cannot be later than today.')
             msgbox.exec_()
-        else:
+        else:  # check possible periods of stay overlapping and adding stay to list of periods of stay (dates)
             days_overlapping = 0
             for stay in self.dates:
                 range_of_stay = pd.date_range(start=stay[0], end=stay[1])
@@ -131,28 +127,25 @@ class Ui_MainWindow(object):
                 msgbox.setText('Durations of stay cannot overlap.')
                 msgbox.exec_()
                 return False
-        # Add dates to the list
+        # add period of stay to the dates list
         self.dates.append([self.dateOfEntryPy, self.dateOfExitPy])
         self.dates.sort()
         return True
 
     def printToList(self):
         if self.getDates() is True:
-            self.listWidget.addItem('Entry on {} and exit on {}.\t\t({} -> {})'
-                                    .format(self.dateIn, self.dateOut, self.dateOfEntryPy, self.dateOfExitPy))
+            self.listWidget.addItem('Entry on {} and exit on {}'.format(self.dateIn, self.dateOut))
             self.listWidget.sortItems()
 
     def removeStayAction(self):
-        # Error handling for when there are no items on the list
+        # error index range handling for when there are no items on the list
         try:
             self.listWidget.takeItem(self.listWidget.currentRow())
             self.dates.remove(self.dates[self.listWidget.currentRow()])
         except IndexError:
             pass
 
-    #  Create ranges out of the entry and exit dates
-
-    def createRange(self, list):
+    def createRange(self, list):  # create ranges out of the entry and exit dates
         rangeofdates = pd.date_range(start=list[0], end=list[1])
         return rangeofdates
 
@@ -183,11 +176,10 @@ class Ui_MainWindow(object):
         except:
             self.listWidget_2.clear()
 
-
     def nextPossibleEntry(self):
         self.calculateStay()
         self.listWidget_3.clear()
-        number_loops = 0
+        number_loops = 0  # for selecting position in the list dates
         for stay in self.dates:
             number_loops += 1
             range_forecast = pd.date_range(start=stay[1], end=stay[1] + datetime.timedelta(180))
@@ -203,8 +195,14 @@ class Ui_MainWindow(object):
                     self.listWidget_3.addItem('From the {} you have a total \nof {} days of stay allowed\n'
                                               .format(str(stay[1]+datetime.timedelta(180)), 91-total_days_forecast))
             except IndexError:
+                print(IndexError)
                 pass
 
+    def reset_all_entries(self):
+        self.dates = []
+        self.listWidget.clear()
+        self.listWidget_2.clear()
+        self.listWidget_3.clear()
 
 if __name__ == "__main__":
     import sys
